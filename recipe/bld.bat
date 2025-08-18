e85bda0f9146b9a689d6214622b7f6f312254227
Valentyna Khomits <vkhomits@anaconda.com>
2025-08-18 15:36:46 +0300

    Update version to 4.4.3

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,5 @@
 %PYTHON% -m pip install . -vv --no-deps --no-build-isolation
 if errorlevel 1 exit 1
 
-%PYTHON% setup.py tests
+%PYTHON% setup.py tests --verbose-tests --no-download-t1-files
 if errorlevel 1 exit 1
