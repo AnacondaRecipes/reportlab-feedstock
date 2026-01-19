f2a6ebd6b2a2c80be3b69af88dfd6ad13bd5f20b
Patryk Kups <pkups@anaconda.com>
2026-01-19 12:00:58 +0100

    check

--- recipe/bld.bat
+++
@@ -1,6 +0,0 @@
-%PYTHON% -m pip install . -vv --no-deps --no-build-isolation
-if errorlevel 1 exit 1
-
-REM !!!!! runAll.py --> 3221226505 should exit with error !!!!!
-%PYTHON% setup.py tests --verbose-tests --no-download-t1-files --exclude=test_pdfgen_general --exclude=test_pdfgen_callback
-if errorlevel 1 exit 1
